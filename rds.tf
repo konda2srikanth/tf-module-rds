60d0b119e62de669c2c9927176d357760527bf4a
Konda Srikanth <konda2srikanth@gmail.com>
2024-11-21 18:32:20 +0530

    Auto Push

--- rds.tf
+++ rds.tf
@@ -5,8 +5,8 @@ resource "aws_db_instance" "main" {
   engine                 = var.engine
   engine_version         = var.engine_version
   instance_class         = var.instance_class
-  password               = jsondecode(data.vault_generic_secret.rds.data_json).password
-  username               = jsondecode(data.vault_generic_secret.rds.data_json).username
+  password               = ExpenseApp@1
+  username               = root
   parameter_group_name   = aws_db_parameter_group.main.name
   skip_final_snapshot    = true
   db_subnet_group_name   = aws_db_subnet_group.main.name
